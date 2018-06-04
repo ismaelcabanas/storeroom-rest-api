6e453faeaebd83bae043f89865aed72a2b32d221
Ismael Cabañas García <ismaelcabanas@gmail.com>
2018-06-04 13:45:42 +0200

    Check API health

---
+++ src/main/java/cabanas/garcia/ismael/storeroom/api/Application.java
@@ -0,0 +1,18 @@
+package cabanas.garcia.ismael.storeroom.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+@SuppressWarnings("PMD.UseUtilityClass")
+public class Application {
+  public static void main(String[] args) {
+    run(args);
+  }
+
+  public static ConfigurableApplicationContext run(String[] args) {
+    return SpringApplication.run(Application.class, args);
+  }
+
+}
