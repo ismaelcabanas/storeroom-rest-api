6abf4210fa49163e1a65b46ad6c41c1b3c56c22a
Ismael Cabañas García <ismaelcabanas@gmail.com>
2018-11-19 07:57:03 +0100

    Unit test to check Stock is a Value Object

--- src/test/java/cabanas/garcia/ismael/storeroom/module/storeroom/domain/StockShould.java
+++ src/test/java/cabanas/garcia/ismael/storeroom/module/storeroom/domain/StockShould.java
@@ -1,5 +1,6 @@
 package cabanas.garcia.ismael.storeroom.module.storeroom.domain;
 
+import cabanas.garcia.ismael.shared.domain.ValueObject;
 import cabanas.garcia.ismael.storeroom.module.storeroom.domain.stubs.QuantityStub;
 import cabanas.garcia.ismael.storeroom.module.storeroom.domain.stubs.StockStub;
 import nl.jqno.equalsverifier.EqualsVerifier;
@@ -8,6 +9,17 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StockShould {
+
+  @Test
+  public void be_a_value_object() {
+    // when
+    Stock stock = Stock.builder().withValue(5).build();
+
+    // then
+    assertThat(stock).isInstanceOf(ValueObject.class);
+
+  }
+
   @Test
   public void equalsContract() {
     EqualsVerifier.forClass(Stock.class).verify();
